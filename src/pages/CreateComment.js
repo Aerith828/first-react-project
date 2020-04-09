d493baf2e7ba7cbc9f3cf98c6a3e945d34f66ce1
Aerith828 <58862394+Aerith828@users.noreply.github.com>
2020-04-09 11:16:35 +0200

    fixes on CreateComment.js and DeleteComment.js

--- src/pages/CreateComment.js
+++ src/pages/CreateComment.js
@@ -1,28 +1,58 @@
 import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 const CreateComment = () => {
-    const [idArticle, setidArticle] = useState("");
+    const [article_id, setArticle_id] = useState("");
     const [content, setContent] = useState("");
     const [author, setAuthor] = useState("");
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log("Id Article : ", idArticle);
-        console.log("content : ", content);
-        console.log("author : ", author);
-    }
+
+        fetch('http://localhost:3001/api/comments/create', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                article_id,
+                content,
+                author,
+            }),
+        })
+            .then((result) => {
+                return result.json();
+            })
+            .then(({ status, extra }) => {
+                if (status === "OK") {
+                    setArticle_id("");
+                    setContent("");
+                    setAuthor("");
+                    toast.success("Le commentaire a bien été ajouté");
+                } else {
+                    toast.error(
+                        <div>
+                            Oups ... Nous avons eu une erreur ! <br />
+                            {extra}
+                        </div>
+                    );
+                }
+            })
+            .catch((error) => {
+                toast.error("Oups ... Nous avons eu une erreur !");
+                console.log(error);
+            });
+    };
 
     const handleChange = (event) => {
-        console.log("target name : ", event.target.name);
-        console.log("target value : ", event.target.value);
-
         switch (event.target.name) {
-            case "idArticle":
-                setidArticle(event.target.value);
+            case "article_id":
+                setArticle_id(event.target.value);
                 break;
             case "content":
                 setContent(event.target.value);
@@ -37,14 +67,14 @@ const CreateComment = () => {
     return (
         <Container>
             <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="comment.idArticle">
+                <Form.Group controlId="comment.article_id">
                     <Form.Label>Id de l'article du commentaire</Form.Label>
                     <Form.Control
                         type="number"
-                        name="idArticle"
+                        name="article_id"
                         onChange={handleChange}
-                        value={idArticle}
-                        placeholder="Id de l'article" />
+                        value={article_id}
+                        placeholder="Id de l'article à commenter" />
                 </Form.Group>
                 <Form.Group controlId="comment.content">
                     <Form.Label>Contenu du commentaire</Form.Label>
