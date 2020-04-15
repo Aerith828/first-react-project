fb1f90879a279d5c1df4b8b58c5fdf5284c8a910
Aerith828 <58862394+Aerith828@users.noreply.github.com>
2020-04-15 14:38:08 +0200

    add signin page

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ import 'react-toastify/dist/ReactToastify.min.css';
 import Navigation from './components/Navigation';
 
 import Home          from './pages/Home';
+import Signin        from './pages/Signin';
 import CreateArticle from './pages/CreateArticle';
 import DeleteArticle from './pages/DeleteArticle';
-import ViewArticle from './pages/ViewArticle';
+import ViewArticle   from './pages/ViewArticle';
 import NotFound      from './pages/NotFound';
 
 const App = () => {
@@ -21,9 +22,10 @@ const App = () => {
       <Navigation />
       <Switch>
         <Route exact path="/"          component={Home}          />
+        <Route path="/signin"           component={Signin}        />
         <Route path="/articles/create" component={CreateArticle} />
         <Route path="/articles/delete" component={DeleteArticle} />
-        <Route path="/article/:id"     component={ViewArticle} />
+        <Route path="/article/:id"     component={ViewArticle}   />
         <Route path="*"                component={NotFound}      />
       </Switch>
       <ToastContainer/>
